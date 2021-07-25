cd806bfceba654104d5ee9f03d9524d8e7b90f02
allan frança dutram <allanfd12@gmail.com>
2021-07-25 14:22:25 -0300

    validação cruzada do aprendizado

--- breast_cancer_validacao_cruzada/breast_cancer_validacao_cruzada.py
+++ breast_cancer_validacao_cruzada/breast_cancer_validacao_cruzada.py
@@ -1,53 +1,30 @@
 import pandas as pd
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
 
 #importa os dados e suas saidas
 previsores =  pd.read_csv('../breast_cancer_dataset/entradas_breast.csv')
 classe = pd.read_csv('../breast_cancer_dataset/saidas_breast.csv')
 
-from sklearn.model_selection import train_test_split
-previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores,classe, test_size =0.25)
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
+def criarRede():
+    classificador = Sequential()    
+    classificador.add(Dense(units = 16, activation='relu', kernel_initializer='random_uniform', input_dim = 30))
+    classificador.add(Dense(units = 32, activation='relu', kernel_initializer='random_uniform'))
+    classificador.add(Dense(units = 1, activation='sigmoid'))
+    otimizador = keras.optimizers.Adam(lr=0.001, decay=0.0001, clipvalue=0.5)
+    classificador.compile(optimizer=otimizador, loss='binary_crossentropy', metrics=['binary_accuracy'])
+    return classificador
+    
+classificador = KerasClassifier(build_fn=criarRede,epochs=100, batch_size=10)
 
-classificador = Sequential()
-# formula de partida para o units
-# entradas + saidas /2
-# (30+1)/2 = 15,5 => 16 
-# primeira camada oculta
-classificador.add(Dense(units = 16, activation='relu', 
-                        kernel_initializer='random_uniform', input_dim = 30))
-classificador.add(Dense(units = 16, activation='relu', 
-                        kernel_initializer='random_uniform'))
-# camada de saida
-classificador.add(Dense(units = 1, activation='sigmoid'))
+# resultados por fatia da base de dados
+resultado = cross_val_score(estimator=classificador, X = previsores,y = classe, cv = 10, scoring="accuracy")
 
-otimizador = keras.optimizers.Adam(lr=0.0001, decay=0.00001, clipvalue=0.5)
-
-classificador.compile(optimizer=otimizador, loss='binary_crossentropy',
-                      metrics=['binary_accuracy'])
-
-# cria a rede e define alguns parametros de treinamento
-#classificador.compile(optimizer='adam', loss='binary_crossentropy',
-#                      metrics=['binary_accuracy'])
-
-# batch_size = numero de registros até atualizar pesos
-# epochs = numero de ciclos completos de interação com os dados
-classificador.fit(previsores_treinamento,classe_treinamento, batch_size=10, epochs=100)
-
-#lÊ os pesos
-pesos0 = classificador.layers[0].get_weights()
-pesos1 = classificador.layers[1].get_weights()
-pesos2 = classificador.layers[2].get_weights()
-
-#executa a rede em uma base de teste
-previsoes = classificador.predict(previsores_teste)
-previsoes = (previsoes >0.5)
-from sklearn.metrics import confusion_matrix, accuracy_score
-#calcula a precisão
-precisao = accuracy_score(classe_teste,previsoes)
-#calcula a matriz de confusão
-matrix = confusion_matrix(classe_teste,previsoes)
-
-resultado = classificador.evaluate(previsores_teste,classe_teste)
+media = resultado.mean() #media das pontuações
+desvio = resultado.std() #desvio
+#desvios grandes podem significar overfiting, super treinamento
