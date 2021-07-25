e70c8d56e50252a97c1f543fd70f9d944325a2c4
allan frança dutram <allanfd12@gmail.com>
2021-07-25 20:03:50 -0300

    Dropout para melhorar o overfitting

--- breast_cancer_validacao_cruzada/breast_cancer_validacao_cruzada.py
+++ breast_cancer_validacao_cruzada/breast_cancer_validacao_cruzada.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 
@@ -14,7 +14,10 @@ classe = pd.read_csv('../breast_cancer_dataset/saidas_breast.csv')
 def criarRede():
     classificador = Sequential()    
     classificador.add(Dense(units = 16, activation='relu', kernel_initializer='random_uniform', input_dim = 30))
+    #Dropout zera alguns valores de entrada para diminuir a chance de overfiting
+    classificador.add(Dropout(0.3))
     classificador.add(Dense(units = 32, activation='relu', kernel_initializer='random_uniform'))
+    classificador.add(Dropout(0.3))
     classificador.add(Dense(units = 1, activation='sigmoid'))
     otimizador = keras.optimizers.Adam(lr=0.001, decay=0.0001, clipvalue=0.5)
     classificador.compile(optimizer=otimizador, loss='binary_crossentropy', metrics=['binary_accuracy'])
