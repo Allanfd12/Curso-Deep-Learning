20ac2d9484181fb9d09b96328fd53b8740a57b60
allan frança dutram <allanfd12@gmail.com>
2021-08-05 23:41:19 -0300

    regressão

--- regressao/autos_regressao_um_valor/autos_regressao_um_valor.py
+++ regressao/autos_regressao_um_valor/autos_regressao_um_valor.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
 
 base = pd.read_csv('../dataset/autos.csv', encoding='ISO-8859-1')
 
@@ -70,3 +72,50 @@ valoresSubstituidos2  = {'vehicleType' : 'limousine','gearbox': 'manuell','model
 
 #substitui os valores nulos pelos valores informado
 base = base.fillna(value = valoresSubstituidos)
+
+#seleciona a fatia da base que nos interessa
+previsores = base.iloc[:,1:13].values
+preco_real =base.iloc[:,0].values
+
+#converte dados 'categoricos' em dados numericos, remove palavras e substitui por numeros
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#labelencoder_previsores = LabelEncoder()
+#previsores[:,0] = labelencoder_previsores.fit_transform(previsores[:,0])
+#previsores[:,1] = labelencoder_previsores.fit_transform(previsores[:,1])
+#previsores[:,3] = labelencoder_previsores.fit_transform(previsores[:,3])
+#previsores[:,5] = labelencoder_previsores.fit_transform(previsores[:,5])
+#previsores[:,8] = labelencoder_previsores.fit_transform(previsores[:,8])
+#previsores[:,9] = labelencoder_previsores.fit_transform(previsores[:,9])
+#previsores[:,10] = labelencoder_previsores.fit_transform(previsores[:,10])
+
+# DEPRECIADO
+#onehotencoder = OneHotEncoder(categorical_features =[0,1,2,3,5,8,9,10])
+
+#realiza as duas etapas anteriores juntas
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer 
+
+onehotencoder = ColumnTransformer([("norm1", OneHotEncoder(),[0,1,3,5,8,9,10])], remainder="passthrough")
+previsores = onehotencoder.fit_transform(previsores)
+
+
+regressor = Sequential()
+regressor.add(Dense(units = 320, activation='relu', input_dim = 321))
+regressor.add(Dense(units = 320, activation='relu'))
+regressor.add(Dense(units = 160, activation='relu'))
+regressor.add(Dense(units = 160, activation='relu'))
+regressor.add(Dense(units = 80, activation='relu'))
+regressor.add(Dense(units = 80, activation='relu'))
+regressor.add(Dense(units = 1, activation='linear'))
+regressor.compile(loss = 'mean_absolute_error', optimizer = 'adam', metrics=['mean_absolute_error'])
+
+regressor.fit(previsores, preco_real, batch_size=300, epochs =5)
+
+
+
+
+
+
+
+
+
