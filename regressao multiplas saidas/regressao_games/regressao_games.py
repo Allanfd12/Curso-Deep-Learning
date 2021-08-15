19ff70ec30cb813cfbc036f75340e3f67e332940
allan frança dutram <allanfd12@gmail.com>
2021-08-15 13:00:18 -0300

    plotagem dos resultados

--- regressao multiplas saidas/regressao_games/regressao_games.py
+++ regressao multiplas saidas/regressao_games/regressao_games.py
@@ -1,8 +1,8 @@
-from sklearn.preprocessing import  OneHotEncoder, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 import pandas as pd
 from keras.layers import Dense, Dropout, Activation, Input
 from keras.models import Model
-from sklearn.compose import ColumnTransformer 
+from sklearn.compose import ColumnTransformer
 
 base = pd.read_csv('../dataset/games.csv')
 
@@ -33,12 +33,74 @@ venda_eu = base.iloc[:, 5].values
 venda_jp = base.iloc[:, 6].values
 
 
-onehotencoder = ColumnTransformer([("norm1", OneHotEncoder(),[0,2,3,8])], remainder="passthrough")
+onehotencoder = ColumnTransformer(
+    [("norm1", OneHotEncoder(), [0, 2, 3, 8])], remainder="passthrough")
 previsores = onehotencoder.fit_transform(previsores).toarray()
-#normalização dos dados, converte todos os valores para uma escala
-#entre 0 e 1, para evitar interferencias relacionadas a escala
+# normalização dos dados, converte todos os valores para uma escala
+# entre 0 e 1, para evitar interferencias relacionadas a escala
 scaler = MinMaxScaler()
 scaler.fit(previsores)
 previsores = scaler.transform(previsores)
 
-#rede neural
+# rede neural
+camada_entrada = Input(shape=(61,))
+camada_oculta1 = Dense(units=32, activation='sigmoid')(camada_entrada)
+camada_oculta2 = Dense(units=32, activation='sigmoid')(camada_oculta1)
+camada_saida1 = Dense(units=1, activation='linear')(camada_oculta2)
+camada_saida2 = Dense(units=1, activation='linear')(camada_oculta2)
+camada_saida3 = Dense(units=1, activation='linear')(camada_oculta2)
+
+regressor = Model(inputs=camada_entrada, outputs=[camada_saida1,camada_saida2,camada_saida3])
+regressor.compile(optimizer='adam',loss='mse')
+
+regressor.fit(previsores,[venda_na, venda_jp,venda_eu], epochs = 5000, batch_size = 100)
+
+previsao_na, previsao_jp, previsao_eu = regressor.predict(previsores)
+
+
+import matplotlib.pyplot as plt
+plt.title("Previsão vs realidade Norte America")
+plt.xlabel("indice")
+plt.ylabel("valor")
+plt.plot(previsao_na, color="red", label='previsao') 
+plt.plot(venda_na, color ="green", label='realidade') 
+plt.show()
+
+plt.title("Previsão vs realidade Japão")
+plt.xlabel("indice")
+plt.ylabel("valor")
+plt.plot(previsao_jp, color="red", label='previsao') 
+plt.plot(venda_jp, color="green", label='realidade') 
+plt.show()
+
+plt.title("Previsão vs realidade União europeia")
+plt.xlabel("indice")
+plt.ylabel("valor")
+plt.plot(previsao_eu, color="red", label='previsao') 
+plt.plot(venda_eu, color="green", label='realidade') 
+plt.show()
+
+
+dif = venda_na-previsao_na[:,0]
+
+plt.title("diferença entre valores na")
+plt.xlabel("indice")
+plt.ylabel("valor")
+plt.plot(dif, color="red", label='realidade - previsao') 
+plt.show()
+
+dif = venda_jp-previsao_jp[:,0]
+
+plt.title("diferença entre valores jp")
+plt.xlabel("indice")
+plt.ylabel("valor")
+plt.plot(dif, color="red", label='realidade - previsao') 
+plt.show()
+
+dif = venda_eu-previsao_eu[:,0]
+
+plt.title("diferença entre valores eu")
+plt.xlabel("indice")
+plt.ylabel("valor")
+plt.plot(dif, color="red", label='realidade - previsao') 
+plt.show()
